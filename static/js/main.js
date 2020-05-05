fdb92abdea48f07e83e536a5753105420f2c9bc6
Alex Drinkwater <aldrinkwater@hotmail.com>
2020-05-05 15:55:03 +0000

    modal now working as intended

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,1 @@
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
 
-xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open("GET", "https://sheet.best/api/sheets/b0eb5319-377e-419f-8c19-0a1bbc62a116");
-
-xhr.send();
